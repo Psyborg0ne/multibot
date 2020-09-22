44b306cc47cad8729dd13eb2985d2b0b4e840259
nikosnull <nikos.nullwork@gmail.com>
2020-09-22 14:51:31 +0300

    UPDATE bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,35 @@
 from discord.ext import commands
+import random
+import discord
 
-TOKEN = 'NzU3OTA3MTc4NzQzOTIyNzQ4.X2nOEA.6M8LmLB-zE4aOboTderx_Ejl1fY'
+random.seed()
+
 bot = commands.Bot(command_prefix='>')
 
 @bot.command()
 async def sqr(ctx, num):
+    await ctx.message.delete()
     await ctx.send(int(num)** 2)
+
+@bot.command()
+async def flip(ctx):
+    await ctx.message.delete()
+    flip_embed = discord.Embed(title="FLIPPING MY NIGGA" , description= f'**{ctx.message.author.name}** has flipped: {random.choice([":+1:" , ":-1:"])}')
+
+    await ctx.send(embed=flip_embed)
+
+@bot.command()
+async def roll(ctx, dice=1):
+    await ctx.message.delete()
+    if(1 <= dice < 10):
+        dice_embed = discord.Embed(title="Rolls" , description="Rolling")
+
+        for i in range(int(dice)):
+            dice_embed.add_field(name="Roll " +str(i+1) , value=random.randint(1,6) , inline=True)
+
+        await ctx.send(embed=kati)
+    else:
+        await ctx.send("Wrong usage, dice should be >= 1 and < 10")
+
 
 bot.run(TOKEN)
