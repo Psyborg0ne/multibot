c997ec85d2dcb4da79762b869b1c30afc0c5a555
psyborg0ne <artinopoulos.orfeas@gmail.com>
2020-09-22 13:35:15 +0300

    Basic bot setup

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("hello orfea")
+from discord.ext import commands
+
+TOKEN = 'NzU3OTA3MTc4NzQzOTIyNzQ4.X2nOEA.6M8LmLB-zE4aOboTderx_Ejl1fY'
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def sqr(ctx, num):
+    await ctx.send(int(num)** 2)
+
+bot.run(TOKEN)
