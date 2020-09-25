dca3e785d6b9294556c55a2282c5e256a5823f38
psyborg0ne <artinopoulos.orfeas@gmail.com>
2020-09-25 18:28:34 +0300

    FIX cog imports

--- cogs/cog_gamble.py
+++ cogs/cog_gamble.py
@@ -1,3 +1,4 @@
+import discord, random
 from discord.ext import commands
 
 class Gambling(commands.Cog, name='Gambling'):
@@ -6,7 +7,7 @@ class Gambling(commands.Cog, name='Gambling'):
 		self.bot = bot
 
 	@commands.command()
-	async def flip(ctx):
+	async def flip(self,ctx):
 		# TODO ? Maybe use global msg.delete on command invoke ?
 		await ctx.message.delete()
 		# TODO Flip for Emoji, need to construct Emojy object not shorthand e.g. :+1:
@@ -16,7 +17,7 @@ class Gambling(commands.Cog, name='Gambling'):
 		await ctx.send(embed=flip_embed)
 
 	@commands.command()
-	async def roll(ctx, dice: int= 1):
+	async def roll(self, ctx, dice: int= 1):
 		await ctx.message.delete()
 		if(1 <= dice < 10):
 			dice_embed = discord.Embed(title="Roll Machine" , description="Rolling")
@@ -24,6 +25,6 @@ class Gambling(commands.Cog, name='Gambling'):
 			for i in range(dice):
 				dice_embed.add_field(name=f"#{i+1}:" , value=random.randint(1,6) , inline=True)
 
-			await ctx.send(embed=kati)
+			await ctx.send(embed=dice_embed)
 		else:
 			await ctx.send("Wrong usage, dice should be >= 1 and < 10")
--- cogs/cog_math.py
+++ cogs/cog_math.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 class Mathematics(commands.Cog, name='Mathematics'):
@@ -8,7 +9,7 @@ class Mathematics(commands.Cog, name='Mathematics'):
 	# TODO Add an eval function for quick mafs
 
 	@commands.command()
-	async def sqr(ctx, num: int):
+	async def sqr(self, ctx, num: int):
 		await ctx.message.delete()
 		# TODO Use msg rm delay on send
 		await ctx.send(num** 2, delete_after=5)
